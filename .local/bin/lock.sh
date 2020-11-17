38f37ee44b7f141483fdcf6e157716eddecdacc7
darmanex <darmanex@gmail.com>
2020-11-17 17:37:44 +0700

    Add remove the bg image

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -6,7 +6,8 @@
 #   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
 
 # set the icon and a temporary location for the screenshot to be stored
-icon="$HOME/Themes/Icons/lock/lock-50.png"
+#icon="$HOME/Themes/Icons/lock/lock-50.png"
+icon="$HOME/Pictures/Icons/1891014.png"
 tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
@@ -17,3 +18,5 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -i "$tmpbg"
+
+rm -rf $tmpbg
