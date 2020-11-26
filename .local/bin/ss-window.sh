27bd2fb1bf3c8cb8f0ad4c12a8bda7c348734845
darmanex <darmanex@gmail.com>
2020-11-27 01:14:19 +0700

    Added screenshot key bindings

---
+++ .local/bin/ss-window.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+SS_DIR=~/Pictures/Screenshots
+mkdir -p $SS_DIR
+sleep 0.2; scrot -ub "$SS_DIR/Screenshot_%Y-%m-%d_at_%H:%M.png"; notify-send "Screenshot taken!" "`date +"%H:%M %a %d %b"`"
