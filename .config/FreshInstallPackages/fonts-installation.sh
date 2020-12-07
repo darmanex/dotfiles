9f404b279a5d60a95182b0f9bbf5ace967d94215
darmanex <darmanex@gmail.com>
2020-12-08 00:25:32 +0700

    Update font

--- .config/FreshInstallPackages/fonts-installation.sh
+++ .config/FreshInstallPackages/fonts-installation.sh
@@ -3,4 +3,4 @@
 #https://wiki.archlinux.org/index.php/Fonts
 #https://wiki.archlinux.org/index.php/Localization/Chinese#Fonts
 #https://wiki.archlinux.org/index.php/Font_Configuration_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)/Chinese_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)
-yay -S termsyn-font siji-git xorg-xfd ttf-tw wqy-microhei wqy-zenhei ttf-arphic-ukai ttf-arphic-uming opendesktop-fonts wqy-bitmapfont ttf-hannom otf-ipafont ttf-vlgothic ttf-mplus ttf-ipa-mona ttf-monapo ttf-sazanami ttf-baekmuk ttf-alee ttf-unfonts-core ttf-nanum ttf-nanumgothic_coding --noconfirm
+yay -S noto-fonts-all nerd-fonts-complete  ttf-font-awesome otf-font-awesome ttf-dejavu termsyn-font siji-git xorg-xfd ttf-tw wqy-microhei wqy-zenhei ttf-arphic-ukai ttf-arphic-uming opendesktop-fonts wqy-bitmapfont ttf-hannom otf-ipafont ttf-vlgothic ttf-mplus ttf-ipa-mona ttf-monapo ttf-sazanami ttf-baekmuk ttf-alee ttf-unfonts-core ttf-nanum ttf-nanumgothic_coding --noconfirm
